92797ee7b55c5d6de76a121c8e1ea61126abacbd
jupl <juan.placencia.512@gmail.com>
2014-11-03 23:01:22 -0600

    Formatting

--- gulp/build.js
+++ gulp/build.js
@@ -15,15 +15,16 @@ gulp.task(rump.taskName('build:less'), function() {
                          rump.configs.main.globs.build.less);
   var destination = path.join(rump.configs.main.paths.destination.root,
                               rump.configs.main.paths.destination.less);
+  var sourceMap = rump.configs.main.styles.sourceMap;
 
   return gulp
-  .src([source].concat(rump.configs.main.globs.global))
-  .pipe((rump.configs.watch ? plumber : util.noop)())
-  .pipe((rump.configs.main.styles.sourceMap ? sourcemaps.init : util.noop)())
-  .pipe(less(rump.configs.less))
-  .pipe(autoprefixer(rump.configs.autoprefixer))
-  .pipe((rump.configs.main.styles.sourceMap ? sourcemaps.write : util.noop)())
-  .pipe(gulp.dest(destination));
+    .src([source].concat(rump.configs.main.globs.global))
+    .pipe((rump.configs.watch ? plumber : util.noop)())
+    .pipe((sourceMap ? sourcemaps.init : util.noop)())
+    .pipe(less(rump.configs.less))
+    .pipe(autoprefixer(rump.configs.autoprefixer))
+    .pipe((sourceMap ? sourcemaps.write : util.noop)())
+    .pipe(gulp.dest(destination));
 });
 
 gulp.tasks[rump.taskName('build')].dep.push(rump.taskName('build:less'));
